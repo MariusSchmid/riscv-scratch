0b87b183d90e70612528a51eb186c4465f9431be
Marius Schmid <mafloschmid@aol.com>
2025-03-10 18:24:02 +0000

    generate register

--- soc/gpio_corsair/run.sh
+++ soc/gpio_corsair/run.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-# set -e
+
+cd ip
+mkdir -p generated
+cp `ls | grep -v generated` generated #copy everything but not itself
+corsair generated
+cd ..
+
+
+set -e
 cd firmware && cmake -B build && cmake --build build
 cd ..
-iverilog src/*.v tb_processor.v -o program
-./program | tee | grep 'ERROR' 
+
+
+
+iverilog src/*.v tb_processor.v  ip/generated/hw/*.v -o program
+set +e
+./program | tee | grep 'ERROR'
 if [ $? == 0 ]; then
    echo "matched"
    exit 1
 fi
+set -e
 GDK_BACKEND=x11 gtkwave tb_processor.vcd --script=waves.tcl
