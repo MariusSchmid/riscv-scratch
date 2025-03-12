a1e99085cc48cb0d52156af0baab4aaf6ac74b3d
Schmid Marius (BST/ESW4) <Marius.Schmid2@bosch-sensortec.com>
2025-03-12 16:06:54 +0000

    simplify the testbench for i2c

--- soc/i2c_uart_gpio/ip/i2c/simple_testbench.v
+++ soc/i2c_uart_gpio/ip/i2c/simple_testbench.v
@@ -14,17 +14,7 @@ module i2c_test_bench();
   reg [7:0] slave_addr, i_data_write;
   reg  [15:0] i_sub_addr;
   reg clk, reset_n, i_sub_len;
-  reg request_transmit;
-  reg [23:0] i_byte_len;
-  wire [7:0] data_out;
-  wire valid_out;
-  wire scl;
-  wire sda;
-  wire req_data_chunk, busy, nack;
 
-  //Values for testing data
-  reg en_sda, test_sda, test_sda_prev;
-  reg start_ind, stop_ind;
   reg [7:0] test_data_in, test_data_out;
 
 
@@ -36,7 +26,7 @@ module i2c_test_bench();
       #(5000000) clk = !clk;  //100MHz clock
   end
 
-  assign sda = en_sda ? test_sda : 1'bz;
+  //   assign sda = en_sda ? test_sda : 1'bz;
 
   integer i;
   initial
@@ -52,34 +42,22 @@ module i2c_test_bench();
     /************** Setup of test *****************/
     // en_sda = 0;
     reset_n = 0;
-    request_transmit = 1'b0;
-    test_data_in = 0;
-    test_data_out = 8'hBE;
-    #500;
+    // test_data_in = 0;
+    // test_data_out = 8'hBE;
+    #1;
     reset_n = 1;
-    #10000;
-
-    /****** 8 bit Sub addr, and 2 byte write ******/
-    $display("******** Write 2 Bytes Test: ********");
-    slave_addr = {I2C_ADDR, 1'b0};
-    i_data_write = 8'hFE;
-    i_sub_addr = 8'h2E;
-    i_sub_len = 1'b0;
-    i_byte_len = 23'd2;
-    @(posedge clk);
-    request_transmit <= 1'b1;
-
+    // #10000;
   end
 
 
 
   i2c_master DUT(.i_clk(clk),                     //input clock to the module @100MHz (or whatever crystal you have on the board)
                  .reset_n(reset_n),               //reset for creating a known start condition
-                 .i_addr_w_rw(slave_addr),        //7 bit address, LSB is the read write bit, with 0 being write, 1 being read
-                 .i_sub_addr(i_sub_addr),         //contains sub addr to send to slave, partition is decided on bit_sel
-                 .i_sub_len(i_sub_len),           //denotes whether working with an 8 bit or 16 bit sub_addr, 0 is 8bit, 1 is 16 bit
-                 .i_byte_len(i_byte_len),         //denotes whether a single or sequential read or write will be performed (denotes number of bytes to read or write)
-                 .i_data_write(i_data_write),     //Data to write if performing write action
+                 .i_addr_w_rw({I2C_ADDR, 1'b0}),        //7 bit address, LSB is the read write bit, with 0 being write, 1 being read
+                 .i_sub_addr(8'h2E),         //contains sub addr to send to slave, partition is decided on bit_sel
+                 .i_sub_len(1'b0),           //denotes whether working with an 8 bit or 16 bit sub_addr, 0 is 8bit, 1 is 16 bit
+                 .i_byte_len(24'd2),         //denotes whether a single or sequential read or write will be performed (denotes number of bytes to read or write)
+                 .i_data_write(8'hFE),     //Data to write if performing write action
                  .req_trans(1'b1)    //denotes when to start a new transaction
                 );
 
